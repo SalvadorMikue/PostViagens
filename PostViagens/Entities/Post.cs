5286aedb2b3310345107782a5522cbe7ef2c10f9
SalvadorMikue <74382725+SalvadorMikue@users.noreply.github.com>
2021-01-20 00:19:12 +0000

    Add project files.

---
+++ PostViagens/Entities/Post.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PostViagens.Entities;
+
+namespace PostViagens.Entities
+{
+    class Post
+    {
+        
+        public DateTime Momment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+        }
+
+        public Post(DateTime momment, string title, string content, int likes)
+        {
+            Momment = momment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            comments.Add(comment);
+        }
+        public void RemoveComment(Comment comment)
+        {
+            comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append("Like -");
+            sb.AppendLine(Momment.ToString("dd/MM/yyy  HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments:");
+
+            foreach (Comment c in comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ PostViagens/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PostViagens.Entities;
+
+namespace PostViagens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instanciar os comentarios 
+
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow that Awesomw!");
+
+            // instanciar Post
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018  13:05:44"),
+                "Traveling to New Zealand", "Im going to visit this wonderful!", 12);
+
+            // addicionar no post os comentarios c1 e c2
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            // instanciar outros comentarios
+            Comment c3 = new Comment("Good night");
+            Comment c4 = new Comment("May the Force be with");
+
+            // instanciar os post
+
+
+            Post p2 = new Post(
+                DateTime.Parse("28/07/2018  23:14:19"),
+                "Good nigth guy", "see you tomorrow", 5);
+
+            // adicionar no post os comentarios
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            // imprimir o conteudo de cada post com os comentarios
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+
+
+
+
+        }
+    }
+}
